58e3e1fe8d2b1be7a1a667363afbc77e39e89ac1
maahhi <mahi_jfg@rocketmail.com>
2018-12-12 17:55:01 +0330

    login database check done!

--- Auth/login.js
+++ Auth/login.js
@@ -1,8 +1,32 @@
+const mongoose = require('mongoose');
+const User = require('../model/User');
+const errors = require('restify-errors');
+
+
 module.exports = function(req, res, next){
     console.log(req.body);
-    var username = req.body.username;
-    var password = req.body.password;
-    var displayname = req.body.displayname;
-    console.log(username +" "+ password+" "+displayname);
-  res.send("it's login method");
+    var varusername = req.body.username;
+    var varpassword = req.body.password;
+    var vardisplayname = req.body.displayname;
+    console.log(varusername +" "+ varpassword+" "+vardisplayname);
+    let doc = new User();
+    User.findOne({ username: varusername }, function(err, doc) {
+			if (err) {
+        console.error(err);
+        return next(new errors.InternalError(err.message));
+      }
+      console.log(doc)
+      if (doc == null){
+        return next(
+					new errors.InvalidContentError("rahmani khare"),
+        );
+      }
+      if (varpassword != doc.password){
+        console.log("not match");
+        res.send("NOK");
+			  return next(new errors.UnauthorizedError());
+      }
+			res.send("OK");
+			next();
+		});
 };
