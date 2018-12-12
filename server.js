568b81d7c011fb3f230d2a283f03e159c3a83fbb
maahhi <mahi_jfg@rocketmail.com>
2018-12-12 15:31:32 +0330

    login file created , config file created , mongodb added to server

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var restify = require('restify');
-
+var mongodb = require('mongodb').MongoClient;
 restify.plugins = require('restify-plugins');
+const config = require('./config');
 
 
 var server = restify.createServer();
 
 server.post('/signup', require('./signup.js'));
+server.post('/login', require('./login.js'));
 
 server.use(restify.plugins.bodyParser());
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
+
