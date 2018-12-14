c98ea99ba4ffb1ece99146e7e52b530e0662efb5
Unknown <mahi_jfg@rocketmail.com>
2018-12-14 12:46:01 +0330

    contact add completed

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -8,36 +8,51 @@ function contactController() {
     next()
   }
   this.addContact = function (req, res, next) {
-    res.send("HElloz2");
-    var havalid = req.user.id ;
+    var havalid = req.user.id;
     console.log(havalid);
     var palangUsername = req.body.newContact;
     console.log(palangUsername);
-    
-    User.findOne({ username: palangUsername }, function(err, doc) {
-			if (err) {
+
+    User.findOne({ username: palangUsername }, function (err, doc) {
+      if (err) {
         console.error(err);
         return next(new errors.InternalError(err.message));
       }
       console.log("doc:" + doc)
-      if (doc == null){
-        console.log("in if ????????:" )
+      if (doc == null) {
+        console.log("in if ????????:")
         return next(
-					new errors.InvalidContentError("rahmani khare")
+          new errors.InvalidContentError("rahmani khare")
         );
       }
-      var palangID =doc._id;
-      var tmp = new Contact({"h":havalid,"p":palangID})
-      tmp.save(function(err){
-        if (err) {
-          console.error(err);
-          return next(new errors.InternalError(err.message));
+      console.log("ready to save")
+      var palangID = doc._id;
+      var tmp = new Contact({ "h": havalid, "p": palangID })
+      console.log(tmp)
+
+      Contact.find(tmp, function (err2, con) {
+        if (err2) {
+          console.error(err2);
+          return next(new errors.InternalError(err2.message));
         }
-        res.send(201);
-        next();
+        console.log("con:" + con)
+        if (con != null) {
+          return next(new errors.ConflictError("contact exist"))
+        } else {
+          tmp.save(function (err) {
+            if (err) {
+              console.error(err);
+              return next(new errors.InternalError(err.message));
+            }
+            res.send(201);
+            next();
+          });
+        }
       });
+
+
     });
-    
+
   }
 }
 
