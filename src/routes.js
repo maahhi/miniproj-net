9354660c64b3a4a5f0cd00e5cab56b3d7c4b28bd
Unknown <mahi_jfg@rocketmail.com>
2018-12-14 14:35:59 +0330

    room and rooms model and createroom added

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ module.exports = function(server) {
 
   server.post('/signup', require('./Auth/signup.js'));
   server.post('/login', require('./Auth/login.js'));
+  server.post('/creatRoom', require('./createRoom.js'));
 
   server.get('/whoami', (req, res, next) => {
     res.send(req.user);
@@ -11,7 +12,4 @@ module.exports = function(server) {
   contact = require('./Controllers/contactController');
   server.get('/contact', contact.listContacts);
   server.post('/addContact', contact.addContact);
-  
-
-
 };
