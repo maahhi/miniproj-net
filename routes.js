3d896ec27d9c378c209532be49f85d446714a560
gogogy <ehsanpelatini@gmail.com>
2018-12-14 04:21:17 +0330

    whoami endpoint for testing jwt

--- routes.js
+++ routes.js
@@ -3,4 +3,12 @@ module.exports = function(server) {
   server.post('/signup', require('./Auth/signup.js'));
   server.post('/login', require('./Auth/login.js'));
 
+  server.get('/whoami', (req, res, next) => {
+    res.send(req.user);
+    next()
+  });
+
+  // server.get('/contact', require('./Contact/get'));
+  // server.post('/addContact' /* put your callback here*/);
+
 };
