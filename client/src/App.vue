08e0e7ce79ac3abfa6da3211b0cec3a27584bd44
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2018-12-27 23:59:19 +0330

    Transitions in router-view (doesn't work)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,7 @@
 <template>
+  <transition name="slide-left">
   <router-view @authenticated="createSSE"></router-view>
+  </transition>
 </template>
 
 <script>
@@ -36,3 +38,26 @@ export default {
   }
 }
 </script>
+
+<style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s ease;
+  }
+  .fade-enter, .fade-leave-active {
+    opacity: 0;
+  }
+  .child-view {
+    position: absolute;
+    transition: all .5s cubic-bezier(.55,0,.1,1);
+  }
+  .slide-left-enter, .slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(30px, 0);
+    transform: translate(30px, 0);
+  }
+  .slide-left-leave-active, .slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-30px, 0);
+    transform: translate(-30px, 0);
+  }
+</style>
